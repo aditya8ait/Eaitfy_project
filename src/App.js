53a61dc559df54a2027189edef1f35b883248c7d
aditya8ait <adityatiwari_21305@aitpune.edu.in>
2024-05-06 15:39:35 +0530

    finally

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import Card from './components/Card'
-import Category from './components/Category'
-import Food from './components/Food'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -13,9 +10,6 @@ const App = () => {
     <>
       <Navbar />
       <Hero />
-      {/* <Card />
-      <Food />
-      <Category /> */}
       <EndIcon />
       <Footer />
     </>
