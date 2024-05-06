bc59798b226bb2cdc61bb1b74db843017dc11d9f
aditya8ait <adityatiwari_21305@aitpune.edu.in>
2024-05-06 12:57:45 +0530

    commiting changes

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4">
+      Thanks for shopping with us❤️ . Visit Again ✨
+    </footer>
+  );
+};
+
+export default Footer;
